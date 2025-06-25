c356bb9f5aca0443aa40a8d7e9e2d221d145b248
ybq's new mac <binnayao@qq.com>
2025-06-25 13:11:02 +0800

    refactor(薪资模块): 移除废弃的薪资相关API接口和工具定义

--- src/server/index.ts
+++ src/server/index.ts
@@ -31,54 +31,6 @@ export const switchApi = async (request: CallToolRequest) => {
       }
       break;
     }
-    case "salarySetting_querySalarySetting": {
-      response = await client.post(
-        "v2/salarySetting/querySalarySetting",
-        params
-      );
-      break;
-    }
-    case "salarySetting_saveSalaryItemList": {
-      response = await client.post(
-        "v3/salarySetting/saveSalaryItemList",
-        params
-      );
-      break;
-    }
-    case "salaryBasePay_saveOneBasepayConfig": {
-      response = await client.post(
-        "v1/salaryBasePay/saveOneBasepayConfig",
-        params
-      );
-      break;
-    }
-    case "salaryBasePay_queryConfig": {
-      response = await client.post("v1/salaryBasePay/queryConfig", params);
-      break;
-    }
-    case "salaryBasePay_getHrmField": {
-      response = await client.post("v1/salaryBasePay/getHrmField", params);
-      break;
-    }
-    case "salarySetting_getTemSalaryItemByAi": {
-      response = await client.post(
-        "v1/salarySetting/getTemSalaryItemByAi",
-        params
-      );
-      break;
-    }
-    case "salarySetting_saveSalaryItemList_hrm":
-    case "salarySetting_saveSalaryItemList_basePay":
-    case "salarySetting_saveSalaryItemList_act":
-    case "salarySetting_saveSalaryItemList_floatingData":
-    case "salarySetting_saveSalaryItemList_other":
-    case "salarySetting_saveSalaryItemList_socialSecurityData": {
-      response = await client.post(
-        "v3/salarySetting/saveSalaryItemList",
-        params
-      );
-      break;
-    }
     default:
       throw new Error(`Unknown tool: ${request.params.name}`);
   }
